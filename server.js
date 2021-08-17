69e30f1be9c1e4b44a09e96fd431d8e6d3c625b3
IsmaelJefferson <ismael_jefferson181@hotmail.com>
2021-08-17 11:30:00 -0300

    adicionando projeto com Back end

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+
+const server = express();
+
+server.use(express.static("public"));
+
+server.set("view engine" , "njk");
+
+nunjucks.configure("views" , {
+    express: server
+})
+
+
+server.get("/", function(req , res){
+    return res.render("courses");
+})
+
+
+server.get("/about", function(req, res){
+    return res.render("about");
+})
+
+
+server.listen(5000 , function(){
+    console.log("server is runing");
+})
+
+server.use(function(req , res){
+    res.status(404).render("not-found")
+})
